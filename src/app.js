4bf65af7af0d2bceb4a1f7f149f226ceffea0bde
lilileiva <liliana-carmen@hotmail.com>
2023-02-28 20:09:54 -0300

    add cors url

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
+const { CORS_URL } = process.env;
 
 
 //server initialize
@@ -16,7 +17,7 @@ app.use(morgan('dev'))
 
 //access allow
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin: *');
+    res.header('Access-Control-Allow-Origin', CORS_URL);
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
